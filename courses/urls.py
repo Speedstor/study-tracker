736bae9ce1bdb28e6d85c25879d3bba4b2f65464
Speedstor <aldrin.cheung@gmail.com>
2021-11-06 19:21:17 -0700

    added settings html

--- courses/urls.py
+++ courses/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('session/', views.session, name='session'),
     path('list/', views.list, name='list'),
     path('integrations/', views.integrations, name='integrations'),
+    path('settings/', views.settings, name='settings'),
     path('analytics/', views.analytics, name='analytics'),
 ]
